e85d122950e7213e88ba9268da226c17c727bb06
karinabernardin <karinabernardin@gmail.com>
2021-01-02 20:15:14 -0300

    Get location from command line.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,24 @@
 const locationUtils = require('./utils/locationUtils'); 
 const weatherUtils = require('./utils/weatherUtils');
 
+const location = process.argv[2];
 
-const location = 'Londrina';
-
-locationUtils.getCoordinatesFromLocation(location, (error, data) => {
-    if (error) {
-        return console.log(error);
-    }
-
-    weatherUtils.getCurrentWeather(data.latitude, data.longitude, (error, forecastData) => {
+if (!location) {
+    console.log('Please provide a location.');
+} else {
+    locationUtils.getCoordinatesFromLocation(location, (error, data) => {
         if (error) {
             return console.log(error);
         }
+    
+        weatherUtils.getCurrentWeather(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+    
+            console.log(data.location);
+            console.log(forecastData.currentDescription + '. The current temperature is ' + forecastData.currentTemperature + '°C but the temperature sensation is of ' + forecastData.currentFeelsLike + '°C.');
+        });
+    });    
+}
 
-        console.log(data.location);
-        console.log(forecastData.currentDescription + '. The current temperature is ' + forecastData.currentTemperature + '°C but the temperature sensation is of ' + forecastData.currentFeelsLike + '°C.');
-    });
-});
