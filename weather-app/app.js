cda669a6aaa20f63eb331b85033a5cf91391b90d
karinabernardin <karinabernardin@gmail.com>
2021-01-02 18:02:34 -0300

    Refactor app.js.

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,16 +5,16 @@ const weatherUtils = require('./utils/weatherUtils');
 const location = 'Londrina';
 
 locationUtils.getCoordinatesFromLocation(location, (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-    if (!error) {
-        console.log('Location: ' + data.location)
-        weatherUtils.getCurrentWeather(data.latitude, data.longitude, (error, data) => {
-            console.log('Error', error);
-            console.log('Data', data);
-            if (!error) {
-                console.log('Success! ' + data.currentDescription + '. The current temperature is ' + data.currentTemperature + '°C but the temperature sensation is of ' + data.currentFeelsLike + '°C.');
-            }
-        });
-}
+    if (error) {
+        return console.log(error);
+    }
+
+    weatherUtils.getCurrentWeather(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log(data.location);
+        console.log(forecastData.currentDescription + '. The current temperature is ' + forecastData.currentTemperature + '°C but the temperature sensation is of ' + forecastData.currentFeelsLike + '°C.');
+    });
 });
